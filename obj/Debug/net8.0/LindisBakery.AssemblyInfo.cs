ca5529c5fba4dddc70645586cd88b3051b8be376
Mapula Lekganyane <mapulamichellbanda@gmail.com>
2026-03-01 16:36:02 +0200

    done deal

--- obj/Debug/net8.0/LindisBakery.AssemblyInfo.cs
+++ obj/Debug/net8.0/LindisBakery.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LindisBakery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8fb4eb3362fa3542d75d4599494a6b7e2b5923b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3f411d5b1495e5139597dc61dd9fc0128b3cdea")]
 [assembly: System.Reflection.AssemblyProductAttribute("LindisBakery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LindisBakery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
